45446697eaa206b1892c665314d3c75d14a995e3
dasborad <hosseinhajimohammadi@gmail.com>
2024-06-24 13:15:44 +0330

    edit users form

--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -27,133 +27,129 @@ const Users = () => {
   };
 
   return (
-    <div className="user-form-container">
+    <div className="user-form-container p-4 ">
       <div className="form-wrapper">
-        <h2 className="text-center mb-4">تعریف کاربر جدید</h2>
+        <div className="row">
+  <div className="col-xl-6 text-end border bg-light">
+  <div className="row d-flex justify-content-between">
+  <div className="col-xl-12 text-end">
+        <h2 className="text-end fs-2 mb-4 text-info ">تعریف کاربر جدید</h2>
+        </div>
+        </div>
         <form onSubmit={handleSubmit}>
-          <div className="row d-flex justify-contents-between">
-            <div className="col-xl-8">
 
-          <div className=" form-row">
-            <div className="form-group col-md-3">
-              <label htmlFor="firstName">نام</label>
-              <input
-                type="text"
-                className="form-control"
-                id="firstName"
-                name="firstName"
-                value={formData.firstName}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group col-md-3">
-              <label htmlFor="lastName">نام خانوادگی</label>
-              <input
-                type="text"
-                className="form-control"
-                id="lastName"
-                name="lastName"
-                value={formData.lastName}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </div>
-            </div>
-            
-          </div>
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label htmlFor="email">ایمیل</label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group col-md-6">
-              <label htmlFor="phone">شماره تلفن</label>
-              <input
-                type="tel"
-                className="form-control"
-                id="phone"
-                name="phone"
-                value={formData.phone}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </div>
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label htmlFor="password">کلمه عبور</label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group col-md-6">
-              <label htmlFor="confirmPassword">تایید کلمه عبور</label>
-              <input
-                type="password"
-                className="form-control"
-                id="confirmPassword"
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </div>
-          <div className="text-center">
+        <div className="row d-flex justify-content-between">
+  <div className="col-xl-12 text-end">
+    <div className="d-flex justify-content-around">
+      <h4 className='fs-sm'>نام</h4>
+      <h4 className='text-start'>نام خانوادگی</h4>
+    </div>
+    <div className="row g-6">
+      <div className="col">
+        <input
+          type="text"
+          className="form-control"
+          id="firstName"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="col">
+        <input
+          type="text"
+          className="form-control"
+          id="lastName"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+          required
+        />
+      </div>
+    </div>
+  </div>
+</div>
+<div className="row d-flex justify-content-between mt-4">
+  <div className="col-xl-12 text-end">
+    <div className="d-flex justify-content-around">
+      <h4 className='fs-sm'>ایمیل</h4>
+      <h4 className='text-start'>محل خدمت</h4>
+    </div>
+    <div className="row g-6">
+      <div className="col">
+        <input
+          type="email"
+          className="form-control text-start"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="col">
+        <input
+          type="tel"
+          className="form-control text-start"
+          id="phone"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
+      </div>
+    </div>
+  </div>
+</div>
+<div className="row d-flex justify-content-between mt-4">
+  <div className="col-xl-12 text-end">
+    <div className="d-flex justify-content-around">
+      <h4 className='fs-sm'>کلمه عبور</h4>
+      <h4 className='text-start'>تایید کلمه عبور</h4>
+    </div>
+    <div className="row g-6">
+      <div className="col">
+        <input
+          type="password"
+          className="form-control text-start"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="col">
+        <input
+          type="password"
+          className="form-control text-start"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+      </div>
+    </div>
+  </div>
+</div>
+        </form>
+        <div className="text-center mt-3 mb-2">
             <button type="submit" className="btn btn-primary">
               ارسال
             </button>
           </div>
-        </form>
       </div>
-      <div className="row d-flex justify-content-between">
-  <div className="col-xl-4">
-    <div className="d-flex justify-content-between">
-      <h4>نام</h4>
-      <h4>نام خانوادگی</h4>
-    </div>
-    <div className="row">
+      
 
-    <div className="d-flex justify-content-between">
-    <input
-                type="text"
-                className="form-control"
-                id="firstName"
-                name="firstName"
-                value={formData.firstName}
-                onChange={handleChange}
-                required
-                />
-               <input
-                type="text"
-                className="form-control"
-                id="lastName"
-                name="lastName"
-                value={formData.lastName}
-                onChange={handleChange}
-                required
-                />
+
     </div>
-                </div>
-  </div>
-</div>
+          
+        </div>
+    
     </div>
+    
   );
 };
 
